8d44561aa7d723b2fecf5cb92addc4a4edcee293
julianomachadoo <julmachadoramos@gmail.com>
2022-09-27 15:54:18 -0300

    teste da classe main

--- src/test/java/com/github/julianomachadoo/junitmockitoapi/JunitMockitoApiApplicationTests.java
+++ src/test/java/com/github/julianomachadoo/junitmockitoapi/JunitMockitoApiApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JunitMockitoApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		JunitMockitoApiApplication.main(new String[] {});
 	}
 
 }
